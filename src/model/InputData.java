e3b5e42488e3c18211cbbefd834e21a8a2b2eaf5
Michał Murawski <homasha@gmail.com>
2022-09-26 17:13:34 +0200

    Created getters

--- src/model/InputData.java
+++ src/model/InputData.java
@@ -8,4 +8,30 @@ public class InputData {
     private BigDecimal wiborPercent = new BigDecimal(1.73);
     private BigDecimal amount = new BigDecimal(300000);
     private BigDecimal numberInstalments = new BigDecimal(180);
+    private TypeInstalments typeInstalments = TypeInstalments.CONSTANT;
+    private BigDecimal bankMargin = new BigDecimal(1.9);
+
+    public LocalDate getRepaymentStartDay() {
+        return repaymentStartDay;
+    }
+
+    public BigDecimal getWiborPercent() {
+        return wiborPercent;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public BigDecimal getNumberInstalments() {
+        return numberInstalments;
+    }
+
+    public TypeInstalments getTypeInstalments() {
+        return typeInstalments;
+    }
+
+    public BigDecimal getBankMargin() {
+        return bankMargin;
+    }
 }
